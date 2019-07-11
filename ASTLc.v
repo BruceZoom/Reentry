3606752643af402dc2b524b4555e919dcb3a026b
BruceZoom <32293407+BruceZoom@users.noreply.github.com>
2019-07-11 17:41:45 +0800

    implement ceval' with reentry call and 121 label-state mapping

--- ASTLc.v
+++ ASTLc.v
@@ -13,8 +13,21 @@ Inductive com' : Type :=
   | CIf' (l1 l2 : label) (b : bexp) (c1' c2' : com')
   | CWhile' (l1 l2 : label) (b : bexp) (c' : com')
   | CCall' (l1 l2 : label) (f : func) (pv : list aexp)
-  | CReentry' (l1 l2 lr : label) (lf : list func)
+  | CReentry' (l1 l2 : label) (lf : list func)
+  | CReentryCall (l1 l2 : label) (f : func)
 .
+
+Fixpoint com'_to_com (c' : com') : com :=
+  match c' with
+  | CSkip' _ _ => CSkip
+  | CAss' _ _ X a => CAss X a
+  | CSeq' c1' c2' => CSeq (com'_to_com c1') (com'_to_com c2')
+  | CIf' _ _ b c1' c2' => CIf b (com'_to_com c1') (com'_to_com c2')
+  | CWhile' _ _ b c' => CWhile b (com'_to_com c')
+  | CCall' _ _ f pv => CCall f pv
+  | CReentry' _ _ lf => CReentry lf
+  | _ => CSkip
+  end.
 
 Fixpoint get_entry_label (c' : com') : label :=
   match c' with
@@ -24,7 +37,8 @@ Fixpoint get_entry_label (c' : com') : label :=
   | CIf' l1 _ _ _ _ => l1
   | CWhile' l1 _ _ _ => l1
   | CCall' l1 _ _ _ => l1
-  | CReentry' l1 _ _ _ => l1
+  | CReentry' l1 _ _ => l1
+  | CReentryCall l1 _ _ => l1
   end.
 
 Fixpoint get_exit_label (c' : com') : label :=
@@ -35,7 +49,8 @@ Fixpoint get_exit_label (c' : com') : label :=
   | CIf' _ l2 _ _ _ => l2
   | CWhile' _ l2 _ _ => l2
   | CCall' _ l2 _ _ => l2
-  | CReentry' _ l2 _ _ => l2
+  | CReentry' _ l2 _ => l2
+  | CReentryCall _ l2 _ => l2
   end.
 
 Inductive valid_com_label : com' -> Prop :=
@@ -57,13 +72,18 @@ Inductive valid_com_label : com' -> Prop :=
       valid_com_label (CWhile' l1 l2 b c')
   | V_Call' : forall l1 l2 f pv,
       valid_com_label (CCall' l1 l2 f pv)
-  | V_Reentry' : forall l1 l2 lr lf,
-      valid_com_label (CReentry' l1 l2 lr lf)
+  | V_Reentry' : forall l1 l2 lf,
+      valid_com_label (CReentry' l1 l2 lf)
+  | V_ReentryCall : forall l1 l2 f,
+      valid_com_label (CReentryCall l1 l2 f)
 .
 
 Definition func_context' : Type := func -> (list ident) * com' * label.
 
 Definition label_context : Type := label -> com'.
+
+Definition label_func'_context_match (lbc : label_context) (fc' : func_context') : Prop :=
+  forall f, exists l1 l2, lbc (snd (fc' f)) = CReentryCall l1 l2 f.
 
 Inductive ceval' : label_context -> func_context' -> com' -> label -> label -> state -> state -> Prop :=
 (** Elementary Operations *)
@@ -144,32 +164,42 @@ Inductive ceval' : label_context -> func_context' -> com' -> label -> label -> s
 (** [] *)
 
 (** Reentry *)
-  | E_Reentry'12 : forall lbc fc' lf l1 l2 lr st,
-      ceval' lbc fc' (CReentry' l1 l2 lr lf) l1 l2 st st
-  | E_Reentry'1c : forall lbc fc' lf l1 l2 lr f st,
+  (* Transitions between entries and exits *)
+  | E_Reentry'12 : forall lbc fc' lf l1 l2 st,
+      ceval' lbc fc' (CReentry' l1 l2 lf) l1 l2 st st
+  | E_Reentry'1c : forall lbc fc' lf l1 l2 f st,
       In f lf ->
-      ceval' lbc fc' (CReentry' l1 l2 lr lf) l1 (snd (fc' f)) st st
-  | E_Reentry'r2 : forall lbc fc' lf l1 l2 lr f st,
+      ceval' lbc fc' (CReentry' l1 l2 lf) l1 (snd (fc' f)) st st
+  | E_Reentry'r2 : forall lbc fc' lf l1 l2 f st,
       In f lf ->
-      ceval' lbc fc' (CReentry' l1 l2 lr lf) (get_exit_label (lbc (snd (fc' f)))) l2 st st
-  | E_Reentry'rc : forall lbc fc' lf l1 l2 lr f1 f2 st,
+      ceval' lbc fc' (CReentry' l1 l2 lf) (get_exit_label (lbc (snd (fc' f)))) l2 st st
+  | E_Reentry'rc : forall lbc fc' lf l1 l2 f1 f2 st,
       In f1 lf ->
       In f2 lf ->
-      ceval' lbc fc' (CReentry' l1 l2 lr lf) (get_exit_label (lbc (snd (fc' f1)))) (snd (fc' f2)) st st
+      ceval' lbc fc' (CReentry' l1 l2 lf) (get_exit_label (lbc (snd (fc' f1)))) (snd (fc' f2)) st st
+
+  (* Concrete function calls *)
+  | E_ReentryCall : forall lbc fc' f pv l1 l2 loc1 glb1 glb2,
+      (exists loc2,
+        ceval' lbc fc' (snd (fst (fc' f))) (get_entry_label (snd (fst (fc' f)))) (get_exit_label (snd (fst (fc' f)))) ((param_to_local_state (loc1, glb1) (fst (fst (fc' f))) pv), glb1) (loc2, glb2)) ->
+      ceval' lbc fc' (CReentryCall l1 l2 f) l1 l2 (loc1, glb1) (loc1, glb2)
+
+  (* Label continuation *)
+  | E_LC : forall lbc fc' c' l1 l2 l3 st1 st2 st3,
+      ceval' lbc fc' c' l1 l3 st1 st3 ->
+      ceval' lbc fc' (lbc l3) l3 l2 st3 st2 ->
+      ceval' lbc fc' c' l1 l2 st1 st2
 (** [] *)
 .
 
-.
 
-  | E_Skip : forall fc st lb,
-      ceval' fc (lb, CSkip) lb (lb + 1) st st
-  | E_Ass : forall fc st X a n lb,
-      ceval' fc (lb, CAss X a) lb (lb + 1) st (update_state st X n)
-  | E_Seq : forall fc c1 c2 st1 st2 st3 lb1 lb2 lb3 lb4,
-      ceval' fc (lb1, c1) lb2 lb3 st1 st2 ->
-      ceval' fc (lb3, c2) lb3 lb4 st2 st3 ->
-      ceval' fc (lb1, CSeq c1 c2) lb2 lb4 st1 st3
-  | E_IfTrue : forall fc b c1 c2 st1 st2 lb1 lb2 lb3.
-      beval st1 b = true ->
-      ceval' fc (lb1, )
-.
+
+
+
+
+
+
+
+
+
+
