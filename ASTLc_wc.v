9d10cc3898118193928c40af854a4e3f8780ca3e
BruceZoom <32293407+BruceZoom@users.noreply.github.com>
2019-10-09 20:10:56 +0800

    adujst ceval' and middle ceval' by separating label calling stack and local state calling stack

--- ASTLc_wc.v
+++ ASTLc_wc.v
@@ -83,14 +83,17 @@ Qed.
 
 
 (** Definition of basic ceval' *)
-(* Definition lsstack : Type := list (label * unit_state). *)
+(* Calling stacks are ordered top-down, i.e. inner call state at stack top *)
+(* label calling stack *)
 Definition lbstk : Type := list label.
+(* local calling stack *)
 Definition lcstk : Type := list unit_state.
+Definition state' : Type := lcstk (* local *) * unit_state (* global *).
 
 Definition pure_stk (bstk : lbstk) : Prop :=
   forall lb, In lb bstk -> is_pure lb.
 
-Inductive ceval' : func_context -> com -> lbstk -> lbstk -> (lcstk * unit_state) -> (lcstk * unit_state) -> Prop :=
+Inductive ceval' : func_context -> com -> lbstk -> lbstk -> state' -> state' -> Prop :=
   | E'_Skip : forall fc loc glb,
       ceval' fc CSkip (LPure :: nil) (LPure :: nil) (loc :: nil, glb) (loc :: nil, glb)
   | E'_Ass : forall fc X a n loc1 loc2 glb1 glb2,
@@ -103,161 +106,244 @@ Inductive ceval' : func_context -> com -> lbstk -> lbstk -> (lcstk * unit_state)
   | E'_Reentryr2 : forall fc loc glb,
       ceval' fc CReentry (LHere :: nil) (LPure :: nil) (loc :: nil, glb) (loc :: nil, glb)
 
-  | E'_CallOut : forall fc f pv stk l2 loc loc2 glb1 glb2 bstk cstk,
+  | E'_CallOut : forall fc f pv l2 loc1 loc2 glb1 glb2 bstk sstk,
       single_point l2 ->
-      length bstk = length cstk ->
+      length bstk = length sstk ->
       ceval' fc (func_bdy f)
         (com_to_lable_pure (func_bdy f) :: nil) (bstk ++ l2 :: nil)
-        (param_to_local_state (loc, glb1) (func_arg f) pv :: nil, glb1)
-        (cstk ++ loc2 :: nil, glb2)
+        (param_to_local_state (loc1, glb1) (func_arg f) pv :: nil, glb1)
+        (sstk ++ loc2 :: nil, glb2) ->
 (** Might be equivalent to
         ((l2, loc2) :: stk) glb1 glb2 -> *)
       ceval' fc (CCall f pv)
-        ((LPure, loc) :: nil) ((LHere, loc) :: stk ++ (l2, loc2) :: nil) glb1 glb2
+        (LPure :: nil) (LHere :: bstk ++ l2 :: nil)
+        (loc1 :: nil, glb1) (loc1 :: sstk ++ loc2 :: nil, glb2)
 (** Might be equivalent to
         ((LPure, loc) :: nil) ((LHere, loc) :: (l2, loc2) :: stk) glb1 glb2 *)
-  | E'_CallRet : forall fc f pv stk l1 loc loc1 loc2 glb1 glb2,
+  | E'_CallRet : forall fc f pv l1 loc loc1 loc2 glb1 glb2 bstk sstk,
       single_point l1 ->
+      length bstk = length sstk ->
       ceval' fc (func_bdy f)
-        (stk ++ (l1, loc1) :: nil) ((com_to_lable_pure (func_bdy f), loc2) :: nil)
-        glb1 glb2 ->
+        (bstk ++ l1 :: nil) ((com_to_lable_pure (func_bdy f)) :: nil)
+        (sstk ++ loc1 :: nil, glb1) (loc2 :: nil, glb2) ->
       ceval' fc (CCall f pv)
-        ((LHere, loc) :: stk ++ (l1, loc1) :: nil) ((LPure, loc) :: nil) glb1 glb2
-  | E'_CallSeg : forall fc f pv l1 l2 stk1 stk2 loc loc1 loc2 glb1 glb2,
+        (LHere :: bstk ++ l1 :: nil) (LPure :: nil)
+        (loc :: sstk ++ loc1 :: nil, glb1) (loc :: nil, glb2)
+  | E'_CallSeg : forall fc f pv l1 l2 loc loc1 loc2 glb1 glb2 bstk1 bstk2 sstk1 sstk2,
       single_point l1 ->
       single_point l2 ->
+      length bstk1 = length sstk1 ->
+      length bstk2 = length sstk2 ->
       ceval' fc (func_bdy f)
-        (stk1 ++ (l1, loc1) :: nil) (stk2 ++ (l2, loc2) :: nil) glb1 glb2 ->
+        (bstk1 ++ l1 :: nil) (bstk2 ++ l2 :: nil)
+        (sstk1 ++ loc1 :: nil, glb1) (sstk2 ++ loc2 :: nil, glb2) ->
       ceval' fc (CCall f pv)
-        ((LHere, loc) :: stk1 ++ (l1, loc1) :: nil)
-        ((LHere, loc) :: stk2 ++ (l2, loc2) :: nil) glb1 glb2
+        (LHere :: bstk1 ++ l1 :: nil) (LHere :: bstk2 ++ l2 :: nil)
+        (loc :: sstk1 ++ loc1 :: nil, glb1) (loc :: sstk2 ++ loc2 :: nil, glb2)
   | E'_CallPure : forall fc f pv loc loc2 glb1 glb2,
       ceval' fc (func_bdy f)
-        ((com_to_lable_pure (func_bdy f),
-          (param_to_local_state (loc, glb1) (func_arg f) pv)) :: nil)
-        ((com_to_lable_pure (func_bdy f), loc2) :: nil) glb1 glb2 ->
-      ceval' fc (CCall f pv) ((LPure, loc) :: nil) ((LPure, loc) :: nil) glb1 glb2
+        ((com_to_lable_pure (func_bdy f)) :: nil) ((com_to_lable_pure (func_bdy f)) :: nil)
+        ((param_to_local_state (loc, glb1) (func_arg f) pv) :: nil, glb1) (loc2 :: nil, glb2) ->
+      ceval' fc (CCall f pv)
+        (LPure :: nil) (LPure :: nil)
+        (loc :: nil, glb1) (loc :: nil, glb2)
 
-  | E'_Seq : forall fc c1 c2 l1 l2 l3 l4 stk1 stk2 stk3 loc1 loc2 loc3 glb1 glb2 glb3,
+  | E'_Seq : forall fc c1 c2 l1 l2 l3 l4 glb1 glb2 st bstk1 bstk2 sstk1 sstk2,
       valid_label l1 ->
       is_pure l2 ->
       is_pure l3 ->
       valid_label l4 ->
-      ceval' fc c1 ((l1, loc1) :: stk1) ((l2, loc3) :: stk3) glb1 glb3 ->
-      ceval' fc c2 ((l3, loc3) :: stk3) ((l4, loc2) :: stk2) glb3 glb2 ->
-(**   Should be equivalent to the following property
-      ceval' fc c1 ((l1, loc1) :: stk1) ((l2, loc3) :: nil) glb1 glb3 ->
-      ceval' fc c2 ((l3, loc3) :: nil) ((l4, loc2) :: stk2) glb3 glb2 -> *)
+      1 + length bstk1 = length sstk1 ->
+      1 + length bstk2 = length sstk2 ->
+      ceval' fc c1
+        (l1 :: bstk1) (l2 :: nil)
+        (sstk1, glb1) st ->
+      ceval' fc c2
+        (l3 :: nil) (l4 :: bstk2)
+        st (sstk2, glb2) ->
       ceval' fc (CSeq c1 c2)
-        ((LSeq l1 l3, loc1) :: stk1) ((LSeq l2 l4, loc2) :: stk2) glb1 glb2
-  | E'_Seq1 : forall fc c1 c2 l1 l2 stk1 stk2 loc1 loc2 glb1 glb2,
+        ((LSeq l1 l3) :: bstk1) ((LSeq l2 l4) :: bstk2)
+        (sstk1, glb1) (sstk2, glb2)
+  | E'_Seq1 : forall fc c1 c2 l1 l2 glb1 glb2 bstk1 bstk2 sstk1 sstk2,
       valid_label l1 ->
       single_point l2 ->
-      ceval' fc c1 ((l1, loc1) :: stk1) ((l2, loc2) :: stk2) glb1 glb2 ->
+      1 + length bstk1 = length sstk1 ->
+      1 + length bstk2 = length sstk2 ->
+      ceval' fc c1
+        (l1 :: bstk1) (l2 :: bstk2)
+        (sstk1, glb1) (sstk2, glb2) ->
       ceval' fc (CSeq c1 c2)
-        ((LSeq l1 (com_to_lable_pure c2), loc1) :: stk1)
-        ((LSeq l2 (com_to_lable_pure c2), loc2) :: stk2) glb1 glb2
-  | E'_Seq2 : forall fc c1 c2 l1 l2 stk1 stk2 loc1 loc2 glb1 glb2,
+        ((LSeq l1 (com_to_lable_pure c2)) :: bstk1)
+        ((LSeq l2 (com_to_lable_pure c2)) :: bstk2)
+        (sstk1, glb1) (sstk2, glb2)
+  | E'_Seq2 : forall fc c1 c2 l1 l2 glb1 glb2 bstk1 bstk2 sstk1 sstk2,
       single_point l1 ->
       valid_label l2 ->
-      ceval' fc c2 ((l1, loc1) :: stk1) ((l2, loc2) :: stk2) glb1 glb2 ->
+      1 + length bstk1 = length sstk1 ->
+      1 + length bstk2 = length sstk2 ->
+      ceval' fc c2
+        (l1 :: bstk1) (l2 :: bstk2)
+        (sstk1, glb1) (sstk2, glb2) ->
       ceval' fc (CSeq c1 c2)
-        ((LSeq (com_to_lable_pure c1) l1, loc1) :: stk1)
-        ((LSeq (com_to_lable_pure c1) l2, loc2) :: stk2) glb1 glb2
+        ((LSeq (com_to_lable_pure c1) l1) :: bstk1)
+        ((LSeq (com_to_lable_pure c1) l2) :: bstk2)
+        (sstk1, glb1) (sstk2, glb2)
 
-  | E'_IfTrue : forall fc b c1 c2 l1 l2 stk loc1 loc2 glb1 glb2,
+  | E'_IfTrue : forall fc b c1 c2 l1 l2 loc1 glb1 glb2 bstk sstk,
       is_pure l1 ->
       valid_label l2 ->
+      1 + length bstk = length sstk ->
       beval (loc1, glb1) b = true ->
-      ceval' fc c1 ((l1, loc1) :: nil) ((l2, loc2) :: stk) glb1 glb2 ->
+      ceval' fc c1
+        (l1 :: nil) (l2 :: bstk)
+        (loc1 :: nil, glb1) (sstk, glb2) ->
       ceval' fc (CIf b c1 c2)
-        ((LIf b l1 (com_to_lable_pure c2), loc1) :: nil)
-        ((LIf b l2 (com_to_lable_pure c2), loc2) :: stk) glb1 glb2
-  | E'_IfFalse : forall fc b c1 c2 l1 l2 stk loc1 loc2 glb1 glb2,
+        ((LIf b l1 (com_to_lable_pure c2)) :: nil)
+        ((LIf b l2 (com_to_lable_pure c2)) :: bstk)
+        (loc1 :: nil, glb1) (sstk, glb2)
+  | E'_IfFalse : forall fc b c1 c2 l1 l2 loc1 glb1 glb2 bstk sstk,
       is_pure l1 ->
       valid_label l2 ->
+      1 + length bstk = length sstk ->
       beval (loc1, glb1) b = false ->
-      ceval' fc c2 ((l1, loc1) :: nil) ((l2, loc2) :: stk) glb1 glb2 ->
+      ceval' fc c2
+        (l1 :: nil) (l2 :: bstk)
+        (loc1 :: nil, glb1) (sstk, glb2) ->
       ceval' fc (CIf b c1 c2)
-        ((LIf b (com_to_lable_pure c1) l1, loc1) :: nil)
-        ((LIf b (com_to_lable_pure c1) l2, loc2) :: stk) glb1 glb2
-  | E'_If1 : forall fc b c1 c2 l1 l2 stk1 stk2 loc1 loc2 glb1 glb2,
+        ((LIf b (com_to_lable_pure c1) l1) :: nil)
+        ((LIf b (com_to_lable_pure c1) l2) :: bstk)
+        (loc1 :: nil, glb1) (sstk, glb2)
+  | E'_If1 : forall fc b c1 c2 l1 l2 glb1 glb2 bstk1 bstk2 sstk1 sstk2,
       single_point l1 ->
       valid_label l2 ->
-      ceval' fc c1 ((l1, loc1) :: stk1) ((l2, loc2) :: stk2) glb1 glb2 ->
+      1 + length bstk1 = length sstk1 ->
+      1 + length bstk2 = length sstk2 ->
+      ceval' fc c1
+        (l1 :: bstk1) (l2 :: bstk2)
+        (sstk1, glb1) (sstk2, glb2) ->
       ceval' fc (CIf b c1 c2)
-        ((LIf b l1 (com_to_lable_pure c2), loc1) :: stk1)
-        ((LIf b l2 (com_to_lable_pure c2), loc2) :: stk2) glb1 glb2
-  | E'_If2 : forall fc b c1 c2 l1 l2 stk1 stk2 loc1 loc2 glb1 glb2,
+        ((LIf b l1 (com_to_lable_pure c2)) :: bstk1)
+        ((LIf b l2 (com_to_lable_pure c2)) :: bstk2)
+        (sstk1, glb1) (sstk2, glb2)
+  | E'_If2 : forall fc b c1 c2 l1 l2 glb1 glb2 bstk1 bstk2 sstk1 sstk2,
       single_point l1 ->
       valid_label l2 ->
-      ceval' fc c2 ((l1, loc1) :: stk1) ((l2, loc2) :: stk2) glb1 glb2 ->
+      1 + length bstk1 = length sstk1 ->
+      1 + length bstk2 = length sstk2 ->
+      ceval' fc c2
+        (l1 :: bstk1) (l2 :: bstk2)
+        (sstk1, glb1) (sstk2, glb2) ->
       ceval' fc (CIf b c1 c2)
-        ((LIf b (com_to_lable_pure c1) l1, loc1) :: stk1)
-        ((LIf b (com_to_lable_pure c1) l2, loc2) :: stk2) glb1 glb2
+        ((LIf b (com_to_lable_pure c1) l1) :: bstk1)
+        ((LIf b (com_to_lable_pure c1) l2) :: bstk2)
+        (sstk1, glb1) (sstk2, glb2)
 
   | E'_WhileFalse : forall fc b c loc glb,
       beval (loc, glb) b = false ->
       ceval' fc (CWhile b c)
-        ((LWhile b (com_to_lable_pure c), loc) :: nil)
-        ((LWhile b (com_to_lable_pure c), loc) :: nil) glb glb
-  | E'_WhileTrue1 : forall fc b c l1 l2 stk loc1 loc2 glb1 glb2,
+        ((LWhile b (com_to_lable_pure c)) :: nil)
+        ((LWhile b (com_to_lable_pure c)) :: nil)
+        (loc :: nil, glb) (loc :: nil, glb)
+  | E'_WhileTrue1 : forall fc b c l1 l2 loc1 glb1 glb2 bstk sstk,
       is_pure l1 ->
       single_point l2 ->
+      1 + length bstk = length sstk ->
       beval (loc1, glb1) b = true ->
-      ceval' fc c ((l1, loc1) :: nil) ((l2, loc2) :: stk) glb1 glb2 ->
+      ceval' fc c
+        (l1 :: nil) (l2 :: bstk)
+        (loc1 :: nil, glb1) (sstk, glb2) ->
       ceval' fc (CWhile b c)
-        ((LWhile b l1, loc1) :: nil) ((LWhile b l2, loc2) :: stk) glb1 glb2
-  | E'_WhileTrue2 : forall fc b c l1 l2 stk loc1 loc2 loc3 glb1 glb2 glb3,
+        ((LWhile b l1) :: nil) ((LWhile b l2) :: bstk)
+        (loc1 :: nil, glb1) (sstk, glb2)
+  | E'_WhileTrue2 : forall fc b c l1 l2 loc1 glb1 glb2 bstk sstk st,
       is_pure l1 ->
       valid_label l2 ->
+      1 + length bstk = length sstk ->
       beval (loc1, glb1) b = true ->
       ceval' fc c
-        ((com_to_lable_pure c, loc1) :: nil)
-        ((com_to_lable_pure c, loc3) :: nil) glb1 glb3 ->
+        ((com_to_lable_pure c) :: nil)
+        ((com_to_lable_pure c) :: nil)
+        (loc1 :: nil, glb1) st ->
       ceval' fc (CWhile b c)
-        ((l1, loc3) :: nil) ((l2, loc2) :: stk) glb3 glb2 ->
+        (l1 :: nil) (l2 :: bstk)
+        st (sstk, glb2) ->
       ceval' fc (CWhile b c)
-        ((l1, loc1) :: nil) ((l2, loc2) :: stk) glb1 glb2
-  | E'_WhileSeg1 : forall fc b c l1 l2 stk1 stk2 loc1 loc2 glb1 glb2,
+        (l1 :: nil) (l2 :: bstk)
+        (loc1 :: nil, glb1) (sstk, glb2)
+  | E'_WhileSeg1 : forall fc b c l1 l2 glb1 glb2 bstk1 bstk2 sstk1 sstk2,
       single_point l1 ->
       single_point l2 ->
-      ceval' fc c ((l1, loc1) :: stk1) ((l2, loc2) :: stk2) glb1 glb2 ->
+      1 + length bstk1 = length sstk1 ->
+      1 + length bstk2 = length sstk2 ->
+      ceval' fc c
+        (l1 :: bstk1) (l2 :: bstk2)
+        (sstk1, glb1) (sstk2, glb2) ->
       ceval' fc (CWhile b c)
-        ((LWhile b l1, loc1) :: stk1) ((LWhile b l2, loc2) :: stk2) glb1 glb2
-  | E'_WhileSeg2 : forall fc b c l1 l2 stk1 stk2 loc1 loc2 loc3 glb1 glb2 glb3,
+        ((LWhile b l1) :: bstk1) ((LWhile b l2) :: bstk2)
+        (sstk1, glb1) (sstk2, glb2)
+  | E'_WhileSeg2 : forall fc b c l1 l2 glb1 glb2 bstk1 bstk2 sstk1 sstk2 st,
       single_point l1 ->
       valid_label l2 ->
+      1 + length bstk1 = length sstk1 ->
+      1 + length bstk2 = length sstk2 ->
       ceval' fc c
-        ((l1, loc1) :: stk1) ((com_to_lable_pure c, loc3) :: nil) glb1 glb3 ->
+        (l1 :: bstk1) ((com_to_lable_pure c) :: nil)
+        (sstk1, glb1) st ->
       ceval' fc (CWhile b c)
-        ((LWhile b (com_to_lable_pure c), loc3) :: nil)
-        ((l2, loc2) :: stk2) glb3 glb2 ->
-      ceval' fc (CWhile b c) ((l1, loc1) :: stk1) ((l2, loc2) :: stk2) glb1 glb2
+        ((LWhile b (com_to_lable_pure c)) :: nil) (l2 :: bstk2)
+        st (sstk2, glb2) ->
+      ceval' fc (CWhile b c)
+        (l1 :: bstk1) (l2 :: bstk2)
+        (sstk1, glb1) (sstk2, glb2)
 .
+
+Lemma ceval'_depth_valid : forall fc c bstk1 bstk2 sstk1 sstk2 glb1 glb2,
+  ceval' fc c bstk1 bstk2 (sstk1, glb1) (sstk2, glb2) ->
+  length bstk1 = length sstk1 /\ length bstk2 = length sstk2.
+Proof.
+  intros.
+  remember c as c'.
+  inversion H; subst; auto.
+  - simpl. repeat rewrite app_length.
+    rewrite H9. auto.
+  - simpl. repeat rewrite app_length.
+    rewrite H9. auto.
+  - simpl. repeat rewrite app_length.
+    rewrite H10, H11. auto.
+Qed.
 (** [] *)
 
 
 (** Bridging basic ceval' to multi_ceval' *)
-Inductive middle_ceval' : func_context -> list (func * lsstack * unit_state) -> list (func * lsstack * unit_state) -> Prop :=
-  | ME_r : forall fc f lsstk1 lsstk2 glb1 glb2 stk,
-      ceval' fc (snd (fc f)) lsstk1 lsstk2 glb1 glb2 ->
-      middle_ceval' fc ((f, lsstk1, glb1) :: stk) ((f, lsstk2, glb2) :: stk)
-  | ME_re : forall fc f1 f2 l1 loc1 loc2 glb stk lsstk,
-      single_point l1 ->
-      middle_ceval' fc
-        ((f1, (l1, loc1) :: lsstk, glb)
-          :: stk)
-        ((f2, (com_to_lable_pure (snd (fc f2)), loc2) :: (l1, loc1) :: lsstk, glb)
-          :: (f1, (l1, loc1) :: lsstk, glb)
-          :: stk)
-  | ME_ex : forall fc f1 f2 l2 loc1 loc2 glb1 glb2 stk lsstk,
-      middle_ceval' fc
-        ((f1, (com_to_lable_pure (snd (fc f1)), loc1) :: (l2, loc2) :: lsstk, glb1)
-          :: (f2, (l2, loc2) :: lsstk, glb2)
-          :: stk)
-        ((f2, (l2, loc2) :: lsstk, glb1)
-          :: stk).
+Definition restk : Type := list (com * option lbstk * state').
 
-Definition multi_ceval' (fc : func_context) : list (func * lsstack * unit_state) -> list (func * lsstack * unit_state) -> Prop := clos_refl_trans (middle_ceval' fc).
+Inductive middle_ceval' : func_context -> list func -> restk -> restk -> Prop :=
+  | ME_r : forall fc lf c bstk st1 st2 stk,
+      ceval' fc c bstk ((com_to_lable_pure c) :: nil) st1 st2 ->
+      middle_ceval' fc lf ((c, Some bstk, st1) :: stk) ((c, None, st2) :: stk)
+  | ME_r_single : forall fc c l2 bstk1 bstk2 st1 st2 stk lf,
+      single_point l2 ->
+      ceval' fc c bstk1 (l2 :: bstk2) st1 st2 ->
+      middle_ceval' fc lf ((c, Some bstk1, st1) :: stk) ((c, Some (l2 :: bstk2), st2) :: stk)
+  | ME_re : forall fc c1 c2 l1 loc glb stk lf f bstk sstk,
+      In f lf ->
+      c2 = func_bdy f ->
+      single_point l1 ->
+      (* two calling stacks should match *)
+      1 + length bstk = length sstk ->
+      middle_ceval' fc lf
+        ((c1, Some (l1 :: bstk), (sstk, glb)) :: stk)
+        (* Each reentry call clears the calling stack *)
+        ((c2, Some ((com_to_lable_pure c2) :: nil), (loc :: nil, glb))
+          :: (c1, Some (l1 :: bstk), (sstk, glb)) :: stk)
+  | ME_ex : forall fc c1 c2 l2 glb1 glb2 stk lf bstk loc sstk,
+      single_point l2 ->
+      (* two calling stacks should match *)
+      1 + length bstk = length sstk ->
+      middle_ceval' fc lf
+        ((c1, None, (loc :: nil, glb1))
+          :: (c2, Some (l2 :: bstk), (sstk, glb2)) :: stk)
+        ((c2, Some (l2 :: bstk), (sstk, glb1)) :: stk).
+
+Definition multi_ceval' (fc : func_context) (lf : public_funcs) : restk -> restk -> Prop := clos_refl_trans (middle_ceval' fc lf).
 (** [] *)
